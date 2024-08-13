968572498a3c380ada19fafbb423ec11b3cbfc6e
Matheus Nunes <matheus_gustavo97@hotmail.com>
2024-08-13 19:32:45 -0300

    up

--- Desafio08.py
+++ Desafio08.py
@@ -1,7 +1,7 @@
 #FAÇA UM ALGORITIMO QUE LEIA O PREÇO DE UM PRODUTO E MOSTRE SEU NOVO PREÇO COM 5% DE DESCONTO.
 
 valor = float(input('Digite o valor do produto: '))
-desc = valor - (5/100)
+desc = valor * 5/100
 nvalor = valor - desc
 
 print('O produto com o valor R${:1.0f} Reais, com o desconto de ficará R${} Reais.'.format(valor, nvalor))
