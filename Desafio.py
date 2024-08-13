9a31f7bf90176b1fd6d65b805d34f7acf86e01f9
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2024-08-13 11:08:49 -0300

    novo exercicio

---
+++ Desafio.py
@@ -0,0 +1,6 @@
+n = input('Digite algo:')
+
+print(n)
+print(type(n)) #Pergunta tipo primitivo
+print(n.isupper) #Pergunta se está em MAÍUSCULO
+print(n.isalnum) #Pergunta se é alfaNumerico
--- Soma.py
+++ Soma.py
@@ -1,4 +1,9 @@
-n1 = int(input('Primeiro Número'))
-n2 = int(input('Segundo Número'))
+n1 = int(input('Digite um Número:'))
+n2 = int(input('Digite outro Número:'))
 soma = n1 + n2
-print('A soma é',soma,'!')
+
+print('A soma entre {} e {} vale {}'.format(n1, n2, soma))
+
+#print('A soma entre', n1,'e',n2,'vale {}'.format(soma)) <- maneira não recomendada
+
+#TIPOS PRIMITIVOS INT, STRIN, BOOL, FLOOAT
