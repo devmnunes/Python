28f315960617bacfe6d360fa196e3c5466dce7ea
Matheus Nunes <matheus_gustavo97@hotmail.com>
2024-08-26 21:52:39 -0300

    novos

---
+++ Desafios/DesafioAninhadas01.py
@@ -0,0 +1,19 @@
+#ESCREVA UM PROGRAMA PARA APROVAR UM EMPRÉTIMO BANCÁRIO PARA A COMPRA DE UMA CASA, O PROGRAMA DEVE PERGUNTAR O VALOR DA CASA, O SALÁRIO DO COMPRADOR E EM QUANTOS ANOS ELE VAI PAGA.
+
+#CALCULE O VALOR DA PRESTAÇÃO MENSAL, SABENDO QUE ELA NÃO PODE EXEDER 30% DO SALÁRIO OU ENTÃO O EMPRÉSTIMO SERÁ NEGADO
+
+valor = int(input('Qual o valor do empréstimo: R$ '))
+
+salario = int(input('Qual o valor do seu salário: R$ '))
+
+ano = int(input('Em quantos anos você ira pagar: '))
+
+parcelas = ano * 12
+val_parce = valor / parcelas
+
+print('Com o valor do empréstimo de R${:.2f} , o seu salário de R${:.2f} , e o prazo de {} meses, a parcela do empréstimo é de R${:.2f} '.format(valor, salario, parcelas, val_parce) )
+
+if val_parce <= salario * 30 / 100:
+    print('O empréstimo foi APROVADO!')
+else:
+    print('O empréstimo foi NEGADO!')
---
+++ Desafios/DesafioAninhadas02.py
@@ -0,0 +1,12 @@
+#ESCREVA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO QUALQUER E PEÇA PARA O ÚSUARIO ESCOLHER QUAL SERÁ A BASE DE CONVERSÃO: 
+#1 DECIMAL
+#2 OCTAL
+#3 HEXADECIMAL
+
+num = int(input('Digite um valor: '))
+
+print('[1] Binário')
+print('[2] Octal')
+print('[3] Hexadecimal')
+
+escolha = int(input('qual a base'(0, 3)))
