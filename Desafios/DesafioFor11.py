b0db978103cfa2cb13603b614361ff5e7f2b2933
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2024-09-30 12:46:41 -0300

    Upgrade

--- Desafios/DesafioFor11.py
+++ Desafios/DesafioFor11.py
@@ -1,22 +1,28 @@
 totIdade = 0
+media = 0
 maisVelho = 0
 HomemVelho = 'João'
-for cont in range(1,3):
-    nome = str(input('Nome da {}º pessoa: '.format(cont)))
+mulher = 0
+for cont in range(1,5):
+    nome = str(input('Nome da {}º pessoa: '.format(cont))).strip()
 
     idade = int(input('Idade da {}º pessoa: '.format(cont)))
     totIdade = totIdade + idade
 
+    sexo = str(input('Sexo da {}º pessoa: '.format(cont))).strip()
+
     media = totIdade / cont
 
-    sexo = str(input('Sexo da {}º pessoa: '.format(cont)))
-    listaSexo = sexo.split()
-
-    if sexo == 'Homem':
+    if sexo == 'homem': 
         if idade > maisVelho:
+            maisVelho = idade
             HomemVelho = nome
     
 
+    if sexo == 'mulher'and idade < 20:
+        mulher = mulher + 1
+
 print('A média de idade do grupo é {} anos. '.format(media))
-print('O homem mais velho é o {}, que tem {} anos.'.format(HomemVelho, idade))
-if gggg
+print('O homem mais velho é o {}, que tem {} anos.'.format(HomemVelho, maisVelho))
+print('No total {} Mulheres tem menos de 20 anos'.format(mulher))
+
