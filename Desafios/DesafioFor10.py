941324707ff61f21b01523bb943f1dd984a867b5
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2024-09-30 12:47:50 -0300

    upgrade

--- Desafios/DesafioFor10.py
+++ Desafios/DesafioFor10.py
@@ -1,6 +1,7 @@
 maior = 0
 menor = 0
 for cont in range(1,6):
+<<<<<<< Updated upstream
     peso = float(input('Digite seu peso da {}º pessoa: '.format(cont)))
     if cont == 1:
         maior = peso
@@ -12,3 +13,15 @@ for cont in range(1,6):
             menor = peso
 print('O maior peso digitado foi {}Kg'.format(maior))
 print('O menor peso digitado foi {}Kg'.format(menor))
+=======
+    peso = float(input('Digite seu peso: '))
+    if cont == 0:
+        maior = peso
+        menor = peso
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor =peso
+print('O maior peso digitado foi {}'.format(maior))
+print('O menor peso digitado foi {}'.format(menor))
+>>>>>>> Stashed changes
