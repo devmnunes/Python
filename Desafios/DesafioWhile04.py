727d00dace6428b70db7d44324fe19b6ee133b4e
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2024-10-24 08:08:38 -0300

    ok

--- Desafios/DesafioWhile04.py
+++ Desafios/DesafioWhile04.py
@@ -1,8 +1,13 @@
 #FAÇA UM PROGRAMA QUE LEIA UM NÚMERO QUALQUER E MOSTRE O SEU FATORIAL.
 #EX: 5! = 5*4*3*2*1=120
-result = 0
+
+cont = 1
+fatorial = 1
 num = int(input('Digite um valor: '))
 
-while num  >  1:
-     num * (num - 1)
-print('O fatorial do valor {}! é {}'.format(num, result))
+while cont <= num:
+     fatorial *= cont
+     cont += 1
+
+print('O resultado da fatoração do valor {} é {}.'.format(num, fatorial))
+
