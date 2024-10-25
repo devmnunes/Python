f7f0e1af609ee902aecf1208f12afe881d5b834f
Matheus Nunes <matheus_gustavo97@hotmail.com>
2024-10-24 21:02:34 -0300

    new

--- Desafios/DesafioWhile02.py
+++ Desafios/DesafioWhile02.py
@@ -2,14 +2,15 @@ import random
 from time import sleep
 
 cont = 0
-sorteio = random.randint(0, 10)
 num = int(input('Em qual número eu estou pensando? '))
 print('PROCESSANDO...')
 sleep(2)
+sorteio = random.randint(1, 5)
 while not sorteio == num:
-    cont += 1
-    print('O número escolhido foi {}, e não o {} , TENTE NOVAMENTE!'.format(sorteio, num))
-    num = int(input('Em qual número eu estou pensando? '))
-if sorteio == num:
-    print('O número escolhido foi {}, PARABÉNS VOCÊ ACERTOU, foram necessárias {} tentativas!!!'.format(sorteio, cont))
+        cont += 1
+        sorteio = random.randint(1, 11)
+        print('O número escolhido foi {}, e não o {} , TENTE NOVAMENTE!'.format(sorteio, num))
+        num = int(input('Em qual número eu estou pensando? '))
+ 
+print('O número escolhido foi {}, PARABÉNS VOCÊ ACERTOU, foram necessárias {} tentativas!!!'.format(sorteio, cont))
     
