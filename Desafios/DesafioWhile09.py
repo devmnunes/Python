47dd6ec4a1b9985053dc850b4fc911f0edeb1595
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2024-10-29 07:58:10 -0300

    novo

--- Desafios/DesafioWhile09.py
+++ Desafios/DesafioWhile09.py
@@ -2,11 +2,12 @@ num = int(input('Digite um número: '))
 resp = str(input('Quer continuar: [S/N]')).strip().upper()
 maior = num
 menor = num
-while resp != 'N':
+while resp == 'S':
     num = int(input('Digite um número: '))
     if num < menor:
         menor = num
     if num > maior:
         maior = num
     resp = str(input('Quer continuar: ')).strip().upper()
-print('O maior valor foi {} e o menor foi {}.'.format(maior, menor))
+    if resp == 'N':
+        print('O maior valor foi {} e o menor foi {}.'.format(maior, menor))
