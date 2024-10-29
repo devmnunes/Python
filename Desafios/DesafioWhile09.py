c481e191a9d3345ed8e475e1495349274db0c662
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2024-10-29 12:46:55 -0300

    fazendo

--- Desafios/DesafioWhile09.py
+++ Desafios/DesafioWhile09.py
@@ -2,12 +2,22 @@ num = int(input('Digite um número: '))
 resp = str(input('Quer continuar: [S/N]')).strip().upper()
 maior = num
 menor = num
+total = cont = media = 0
 while resp == 'S':
     num = int(input('Digite um número: '))
+    cont += 1
+    total += num
     if num < menor:
         menor = num
     if num > maior:
         maior = num
-    resp = str(input('Quer continuar: ')).strip().upper()
+    resp = str(input('Quer continuar: [S/N] ')).strip().upper()
+    if resp != 'NnMm':
+        print('Digite uma resposta valida!!!')
+        resp = str(input('Quer continuar: [S/N] ')).strip().upper()
     if resp == 'N':
-        print('O maior valor foi {} e o menor foi {}.'.format(maior, menor))
+        cont += 1
+        total += num
+media = total / cont
+print('Você digitou {} números e a média foi {}.'.format(cont, media))
+print('O maior valor foi {} e o menor foi {}.'.format(maior, menor))
