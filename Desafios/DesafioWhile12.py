6effe0ad5e6a8f2c9720c9b7ba851403857c7277
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2024-10-31 13:06:21 -0300

    pronto

--- Desafios/DesafioWhile12.py
+++ Desafios/DesafioWhile12.py
@@ -1,11 +1,36 @@
 from random import randint
-soma = 0
+cont = soma = vitoria = 0
+
 print('=-'*40)
 print('VAMOS JOGAR PAR OU IMPAR')
+print('=-'*40)
 
 while True:
+    cont += 1
     num = int(input('Diga um valor: '))
     jogador = str(input('Par ou Ímpar? [P/I] ')).upper().strip()
     computador = randint(0, 10)
-    soma = computador + jogador
-    if jogador == 'P' and computador == 'I'
+    soma = num + computador
+
+    print('-'*60)
+    if soma % 2 == 0:
+        print(f'você jogou {num} e o computador {computador}. Total de {soma} DEU PAR')
+    else:
+        print(f'você jogou {num} e o computador {computador}. Total de {soma} DEU ÍMPAR')
+    print('-'*60)
+
+    if jogador == 'P' and soma % 2 == 0:
+        print('VOCÊ GANHOU')
+        vitoria += 1
+    elif jogador =='I'and soma % 2 == 1:
+        print('VOCÊ GANHOU')
+        vitoria += 1
+    else:
+        print('VOCÊ PERDEU')
+        break
+    print('=-'*40)
+
+print(f'Você ganhou {vitoria} vezes.')
+
+
+     
