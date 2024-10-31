59edfdbebaa30e7746bb1755abc99f6c81a06d24
Matheus Nunes <matheus_gustavo97@hotmail.com>
2024-10-30 21:23:52 -0300

    new

--- Desafios/DesafioWhile11.py
+++ Desafios/DesafioWhile11.py
@@ -1,10 +1,10 @@
 #FAÇA UM PROGRAMA QUE MOSTRE A TABUADA DE VÁRIOS NÚMEROS, UM DE CADA VALOR DIGITADO. O PROGRAMA SERÁ INTERROMPIDO QUANDO O NÚMERO SOLICITADO FOR NEGATIVO.
-
-cont = 0
 while True:
-    num = int(input('Quer ver a tabuada de qual valor? '))
-    print(f'-'*40)
-    while cont < 10:
-        cont += 1
-        resultado = num *cont
-        print(f'{num} x {cont} = {resultado}')
+    num = int(input('Qual tabuada você quer ver? '))
+    if num < 0:
+        break
+    print('-' * 40)
+    for cont in range(1, 11):
+        print(f'{num} x {cont} = {num*cont}')
+    print('-' * 40)
+print('PROGRAMA ENCERRADO')
