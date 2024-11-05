a75458afb72da3bb4e03dfe50c602da6897ee9d3
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2024-11-05 13:03:40 -0300

    ok

--- Desafios/DesafioWhile14.py
+++ Desafios/DesafioWhile14.py
@@ -1,16 +1,24 @@
 print('-'*25)
 print('   ADEGA DO INDIANO')
 print('-'*25)
-tot = prodMil = 0
+tot = totmil = menor = cont = 0
+barato = ''
 while True:
     produto = str(input('Nome do produto: ')).strip()
     valor = float(input('Preço: R$ '))
-    resp = ''
+    cont += 1
+    tot = tot + valor
+    if valor > 1000:
+        totmil += 1
+    if cont == 1 or valor < menor:
+        menor = valor
+        barato = produto
+    resp = ' '
     while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N]')).upper().strip()[0]
-    if resp in 'N':
+    if resp == 'N':
         break
-
-print(f'O total de compra foi R${tot:.2f}')
-print(f'Temos {prodMil} produtos custando mais de R$1000.00 ')
+print(f'O total de compra foi R${tot:10.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1000.00 ')
+print(f'O produto  mais barato foi {barato} e custa R${menor}')
 
