510a5dda77700728394a495211238e505fcdbb44
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2024-11-13 12:45:46 -0300

    ok

--- Desafios/DesafioTuplas04.py
+++ Desafios/DesafioTuplas04.py
@@ -2,15 +2,37 @@
 # 1- QUANTAS VEZES APARECEU O VALOR 9
 # 2- EM QUE POSIÇÃO FOI DIGITADO O PRIMEIRO VALOR 3
 # 3- QUAIS FORAM OS NÚMEROS PARES
-cont = val9 = 0
-valores = []
-while True:
-    cont+= 1
-    num = int(input(f'Digite o {cont}º valor: '))
-    if num == 9:
-        val9 += 1
-    valores.append(num)
-    if cont == 4:
-        break
-valores = tuple(valores)
-print(valores)
+val9 = 0
+tupla_par = ()
+n1 = int(input('Digite o 1º valor: '))
+if n1 == 9:
+    val9 += 1
+if n1 % 2 == 0:
+    tupla_par = (n1)
+n2 = int(input('Digite o 2º valor: '))
+if n2 == 9:
+    val9 += 1
+if n2 % 2 == 0:
+    tupla_par = (n2)
+n3 = int(input('Digite o 3º valor: '))
+if n3 == 9:
+    val9 += 1
+if n3 % 2 == 0:
+    tupla_par = (n3)
+n4 = int(input('Digite o 4º valor: '))
+if n4 == 9:
+    val9 += 1
+if n4 % 2 == 0:
+    tupla_par = (n4)
+tupla = (n1, n2, n3, n4)
+
+print(tupla)
+print(f'O valor 9 aparece {val9} vezes.')
+print(f'O primeiro valor 3 aparece na posição {tupla.index(3)}')
+print(f'Os valores pares são {tupla_par}')
+
+
+
+
+
+
